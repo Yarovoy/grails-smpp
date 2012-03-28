37c21d6788a959b2acf60c5c86afb65032a0be80
Yuriy Yarovoy <yuriy.yarovoy@gmail.com>
2012-03-28 14:41:12 +0400

    Optimize imports in the whole project.

--- test/unit/ru/mfcomm/smpp/SmppServiceTests.groovy
+++ test/unit/ru/mfcomm/smpp/SmppServiceTests.groovy
@@ -2,13 +2,9 @@ package ru.mfcomm.smpp
 
 import grails.test.mixin.TestFor
 
-/**
- * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
- */
 @TestFor(SmppService)
 class SmppServiceTests
 {
-
 	void testSomething()
 	{
 		fail "Implement me"
