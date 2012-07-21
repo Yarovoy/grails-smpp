d4b82b3447d6c7a5b741ea2f85aff5cf4d2b9b36
Yuriy Yarovoy <yuriy.yarovoy@gmail.com>
2012-07-21 23:57:35 +0400

    grails-smpp-3 Clean up BuildConfig.groovy.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -2,34 +2,20 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 grails.project.target.level = 1.6
-//grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 grails.project.dependency.resolution = {
-	// inherit Grails' default dependencies
-	inherits("global") {
-		// uncomment to disable ehcache
-		// excludes 'ehcache'
-	}
-	log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+
+	inherits "global"
+
+	log "warn"
+
 	repositories {
+		mavenCentral()
 		grailsCentral()
-		// uncomment the below to enable remote dependency resolution
-		// from public Maven repositories
-		//mavenCentral()
-		//mavenLocal()
-		//mavenRepo "http://snapshots.repository.codehaus.org"
-		//mavenRepo "http://repository.codehaus.org"
-		//mavenRepo "http://download.java.net/maven/2/"
-		//mavenRepo "http://repository.jboss.com/maven2/"
 	}
+
 	dependencies {
-		// specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-
-		// runtime 'mysql:mysql-connector-java:5.1.5'
-		runtime 'org.slf4j:slf4j-api:1.6.4'
-		runtime 'org.slf4j:slf4j-simple:1.6.4'
-
-		runtime 'com.googlecode.jsmpp:jsmpp:2.1.0'
+		compile 'com.googlecode.jsmpp:jsmpp:2.1.0'
 	}
 
 	plugins {
