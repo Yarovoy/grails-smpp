1691c8db41ff3738339720c71f3154c602e82681
Yuriy Yarovoy <yuriy.yarovoy@gmail.com>
2012-07-21 20:29:53 +0400

    grails-smpp-1 Provide more information in plug-in descriptor

--- SmppGrailsPlugin.groovy
+++ SmppGrailsPlugin.groovy
@@ -1,73 +1,37 @@
+import grails.plugin.smpp.meta.SmppPluginSupport
+
 class SmppGrailsPlugin
 {
-	// the plugin version
-	def version = "0.1"
-	// the version or versions of Grails the plugin is designed for
-	def grailsVersion = "2.0 > *"
-	// the other plugins this plugin depends on
+	def version = '0.1'
+	def grailsVersion = '2.0 > *'
 	def dependsOn = [:]
-	// resources that are excluded from plugin packaging
+	def loadAfter = ['services', 'controllers']
+	def observe = ['services', 'controllers']
+	def watchedResources = ["grails-app/services/**/*Service.groovy", "grails-app/controllers/**/*Controller.groovy"]
 	def pluginExcludes = [
-			"grails-app/views/error.gsp"
+			'grails-app/conf/Config.groovy',
+			'grails-app/conf/DataSource.groovy',
+			'grails-app/conf/UrlMappings.groovy',
+			'grails-app/controllers/**/*Controller.groovy',
+			'grails-app/services/**/*Service.groovy',
+			'grails-app/views/**/*.gsp'
 	]
 
-	// TODO Fill in these fields
-	def title = "Smpp Plugin"
-	// Headline display name of the plugin
-	def author = "Your name"
-	def authorEmail = ""
-	def description = '''\
-Brief summary/description of the plugin.
-'''
+	def title = 'Grails Smpp Plugin'
+	def description = 'Grails SMPP plug-in provides easy-way communicating with SMPP-chanel by taking advantage of JSMPP Java library.'
+	def documentation = 'https://github.com/Yarovoy/grails-smpp/wiki'
+	def author = 'Yuriy Yarovoy'
+	def authorEmail = 'yuriy.yarovoy@gmail.com'
 
-	// URL to the plugin's documentation
-	def documentation = "http://grails.org/plugin/smpp"
+	def developers = [[name: 'Yuriy Yarovoy', email: 'yuriy.yarovoy@gmail.com']]
+	def issueManagement = [system: 'GitHub', url: 'https://github.com/Yarovoy/grails-smpp/issues']
+	def scm = [url: 'https://github.com/Yarovoy/grails-smpp']
 
-	// Extra (optional) plugin metadata
-
-	// License: one of 'APACHE', 'GPL2', 'GPL3'
-	//    def license = "APACHE"
-
-	// Details of company behind the plugin (if there is one)
-	//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
-
-	// Any additional developers beyond the author specified above.
-	//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
-
-	// Location of the plugin's issue tracker.
-	//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
-
-	// Online location of the plugin's browseable source code.
-	//    def scm = [ url: "http://svn.grails-plugins.codehaus.org/browse/grails-plugins/" ]
-
-	def doWithWebDescriptor = { xml ->
-		// TODO Implement additions to web.xml (optional), this event occurs before
-	}
-
-	def doWithSpring = {
-		// TODO Implement runtime spring config (optional)
-	}
-
-	def doWithDynamicMethods = { ctx ->
-		// TODO Implement registering dynamic methods to classes (optional)
-	}
-
-	def doWithApplicationContext = { applicationContext ->
-		// TODO Implement post initialization spring config (optional)
-	}
-
-	def onChange = { event ->
-		// TODO Implement code that is executed when any artefact that this plugin is
-		// watching is modified and reloaded. The event contains: event.source,
-		// event.application, event.manager, event.ctx, and event.plugin.
-	}
-
-	def onConfigChange = { event ->
-		// TODO Implement code that is executed when the project configuration changes.
-		// The event is the same as for 'onChange'.
-	}
-
-	def onShutdown = { event ->
-		// TODO Implement code that is executed when the application shuts down (optional)
-	}
+	def doWithWebDescriptor = SmppPluginSupport.doWithWebDescriptor
+	def doWithSpring = SmppPluginSupport.doWithSpring
+	def doWithDynamicMethods = SmppPluginSupport.doWithDynamicMethods
+	def doWithApplicationContext = SmppPluginSupport.doWithApplicationContext
+	def onChange = SmppPluginSupport.onChange
+	def onConfigChange = SmppPluginSupport.onConfigChange
+	def onShutdown = SmppPluginSupport.onShutdown
 }
