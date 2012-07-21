decc5068f7e3682e97ac6cdfea9707456863c091
Yuriy Yarovoy <yuriy.yarovoy@gmail.com>
2012-07-22 00:00:42 +0400

    grails-smpp-3 Clean up BuildConfig.groovy.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -19,9 +19,6 @@ grails.project.dependency.resolution = {
 	}
 
 	plugins {
-		build(":tomcat:$grailsVersion",
-		      ":release:1.0.0") {
-			export = false
-		}
+		build(":tomcat:$grailsVersion", ":release:1.0.0") { export = false }
 	}
 }
